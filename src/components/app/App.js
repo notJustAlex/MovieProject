aab8fe5a42617509e224c040c768a08a56961d92
Alex <alex.zakablook@gmail.com>
2023-06-26 14:49:17 +0200

    Added dark theme and a hook for theme synchronization with the system

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import {lazy, Suspense} from 'react';
+import { Suspense} from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import SerchPanel from "../serchPanel/SerchPanel";
--- src/components/serchPanel/SerchPanel.js
+++ src/components/serchPanel/SerchPanel.js
@@ -1,11 +1,14 @@
 import {useState, useEffect} from 'react';
 import { useNavigate, NavLink, useMatch } from 'react-router-dom';
 
+import {useTheme} from '../../hooks/useTheme';
+
 import './serchPanel.css';
 
 const SerchPanel = () => {
     const [str, setStr] = useState('');
     const navigate = useNavigate();
+    const {theme, setTheme} = useTheme();
 
     useEffect(() => { 
         updateInput();
@@ -23,10 +26,15 @@ const SerchPanel = () => {
         setStr(title);
     }
 
+    const handleLightThemeClick = () => {
+        setTheme(theme => theme === 'light' ? 'dark' : 'light')
+    }
+
     return (
         <div className="search_wrapper">
             <h2 className="search_title" onClick={() => onUpdateInput('')}><NavLink to={'/'}>Find me a movie</NavLink></h2>
             <input value={str} autoComplete="off" id='search_input' className="search_input" type='text' placeholder="E.g. Harry Potter" onChange={(e) => onUpdateInput(e.target.value)}></input>
+            <input type="checkbox" onChange={handleLightThemeClick}/>
         </div>
     )
 }
