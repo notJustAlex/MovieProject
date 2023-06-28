d5869d7ac140771d847406936ec921feee286750
Alex <alex.zakablook@gmail.com>
2023-06-28 19:21:02 +0200

    added HashRouter

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import { Suspense} from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 
 import SerchPanel from "../serchPanel/SerchPanel";
 import MovieList from "../movieLists/MovieList";
