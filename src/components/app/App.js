bfae6b6539aef6b62f83dc938fe1eb7938c34810
Alex <alex.zakablook@gmail.com>
2023-06-29 10:25:39 +0200

    increased loading speed

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,13 @@
-import { Suspense} from 'react';
+import {lazy, Suspense} from 'react';
 import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 
 import SerchPanel from "../serchPanel/SerchPanel";
-import MovieList from "../movieLists/MovieList";
-import SearchMovieList from '../movieLists/SearchMovieList';
-import SingleMoviePage from "../singleMoviePage.js/SingleMoviePage";
 import Spinner from "../spinner/Spinner";
 
-import '../../style/index.css';
+const MovieList = lazy(() => import("../movieLists/MovieList"));
+const SearchMovieList = lazy(() => import("../movieLists/SearchMovieList"));
+const SingleMoviePage = lazy(() => import("../singleMoviePage.js/SingleMoviePage"));
+const ErrorMessage = lazy(() => import("../errorMessage/ErrorMessage"));
 
 const App = () => {
     return (
@@ -20,6 +20,7 @@ const App = () => {
                             <Route exact path='/' element={<MovieList/>}/>
                             <Route path='/search/:str' data='str' element={<SearchMovieList/>}/>
                             <Route path="/movie/:id" element={<SingleMoviePage/>} dataType='movie'/>
+                            <Route path='*' element={<ErrorMessage/>}/>
                         </Routes>
                     </Suspense>
                 </main>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/app/App'; 
 
+import '../src/style/index.css';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
